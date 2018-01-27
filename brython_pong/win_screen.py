40638e8cabf8b05dd02ead2bbc809596b8022b3b
toanh <toan.kien@gmail.com>
2018-01-27 16:52:50 +1100

    updating brython graphics

--- brython_pong/win_screen.py
+++ brython_pong/win_screen.py
@@ -1,4 +1,4 @@
-#Importing the JMSS graphics code 
+#Importing the JMSS graphics code
 from JMSSGraphics_brython import *
 
 #Creating the Graphics Object
@@ -23,7 +23,7 @@ paddle2_lives = 3
 game_state = 0      # 0: title screen, 1: game, 2: win screen
 
 #pre-load our images into RAM (memory)
-ball_image = jmss.loadImage("ball.png")
+ball_image = jmss.loadImage("baby.png")
 paddle1_image = jmss.loadImage("paddle_red.png")
 paddle2_image = jmss.loadImage("paddle_blue.png")
 background_image = jmss.loadImage("background.png")
@@ -31,9 +31,9 @@ title_image = jmss.loadImage("title.png")
 
 #pre-load our sound files
 blip_sound = jmss.loadSound("blip.wav", streaming = False)
-background_music = jmss.loadSound("piano.wav", streaming = True)
+background_music = jmss.loadSound("just_dance.mp3", streaming = True)
 
-#jmss.playSound(background_music, loop = True)
+jmss.playSound(background_music, loop = True)
 
 #The main loop code is below, this gets run once per frame
 @jmss.mainloop
@@ -54,7 +54,7 @@ def Game():
         if paddle1_lives <= 0 or paddle2_lives <= 0:
             # transition to the next win screen state
             game_state = 2
-        else:       
+        else:
             #Code to update my game
             ball_xpos = ball_xpos + ball_xdir
             ball_ypos = ball_ypos + ball_ydir
@@ -67,14 +67,14 @@ def Game():
                 ball_ydir = ball_ydir * -1
 
             #checking for the top edge of the screen
-            if ball_ypos >= 400 - 24:
+            if ball_ypos >= 400 - ball_image.height:
                 #snap it back to the botton
-                ball_ypos = 400 - 24
+                ball_ypos = 400 - ball_image.height
                 #flip its ydir
                 ball_ydir = ball_ydir * -1
 
             #checking for the ball going off the left hand side of the screen
-            if ball_xpos <= -24:
+            if ball_xpos <= -ball_image.width:
                 #reset the position of the ball
                 ball_xpos = 300
                 ball_ypos = 200
@@ -88,7 +88,7 @@ def Game():
 
             #TODO: check for the ball going off the right hand side of the screen
 
-                
+
             #Extension: increase the speed of the ball AFTER it has bounced
             #off each of the edges
 
@@ -129,15 +129,16 @@ def Game():
                 ball_xdir *= -1                 # abbreviation for ball_xdir = ball_xdir * -1
                 jmss.playSound(blip_sound, loop = False)
 
+            print(ball_image.width, ball_image.height)
             #check for collision with the right hand paddle
-            if ball_xpos + 24 >= paddle2_xpos and ball_ypos >= paddle2_ypos and \
-               ball_xpos + 24 <= paddle2_xpos + 24 and ball_ypos <= paddle2_ypos + 144:
+            if ball_xpos + ball_image.width >= paddle2_xpos and ball_ypos >= paddle2_ypos and \
+               ball_xpos + ball_image.width <= paddle2_xpos + 24 and ball_ypos <= paddle2_ypos + 144:
                 # the ball has collided with the left paddle
-                ball_xpos = paddle2_xpos - 24   # snap the ball to the right edge of paddle
+                ball_xpos = paddle2_xpos - ball_image.width   # snap the ball to the right edge of paddle
                                                 # so that the ball does not appear 'inside' the paddle
                 ball_xdir *= -1                 # abbreviation for ball_xdir = ball_xdir * -1
-                jmss.playSound(blip_sound, loop = False)        
-                
+                jmss.playSound(blip_sound, loop = False)
+
 
             #TODO: do the collision for the right hand paddle
             #Note: use the bottom right hand corner of the ball instead of the anchor point
@@ -147,19 +148,19 @@ def Game():
             #extreme angle
             #or... use the direction that the paddle is moving, to induce some 'spin' or directionality
             #to the ball
-                
+
 
             #Code to draw my game
-            jmss.clear(1, 0, 0, 1)
-            #jmss.drawImage(background_image, x = 0, y = 0)
+            jmss.clear()
+            jmss.drawImage(background_image, x = 0, y = 0)
             #the order of drawing means that the paddle is drawn first and
             #will be covered up by the ball if they overlap
             jmss.drawImage(paddle1_image, x = paddle1_xpos, y = paddle1_ypos)
             jmss.drawImage(paddle2_image, x = paddle2_xpos, y = paddle2_ypos)
             jmss.drawImage(ball_image, x = ball_xpos, y = ball_ypos)
 
-#            jmss.drawText("Lives: " + str(paddle1_lives), 10, 350, fontSize=20)
-#            jmss.drawText("Lives: " + str(paddle2_lives), 490, 350, fontSize=20)
+            jmss.drawText("Lives: " + str(paddle1_lives), 10, 350, fontSize=20)
+            jmss.drawText("Lives: " + str(paddle2_lives), 490, 350, fontSize=20)
         #
     # do the logic for the win screen
     elif game_state == 2:
@@ -184,12 +185,12 @@ def Game():
 
             paddle1_lives = 3
             paddle2_lives = 3
-        
+
         # announce our winner
         jmss.clear()
         jmss.drawImage(background_image, x = 0, y = 0)
-#        jmss.drawText("Player " + str(winner) + " wins!!", 170, 200, fontSize = 30)
-        
+        jmss.drawText("Player " + str(winner) + " wins!!", 170, 200, fontSize = 30)
+
 #This starts the Graphics object off and will run the main loop above
 jmss.run()
 
